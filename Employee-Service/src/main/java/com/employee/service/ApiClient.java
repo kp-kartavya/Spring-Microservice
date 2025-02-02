9e1fc860614da3293fd47dc368388df2545a1208
kartavya-p <kartavya.98p@gmail.com>
2025-02-02 15:22:01 +0530

    Eureka CLient

--- Employee-Service/src/main/java/com/employee/service/ApiClient.java
+++ Employee-Service/src/main/java/com/employee/service/ApiClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.employee.dto.DepartmentDto;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+@FeignClient(value = "DEPARTMENT-SERVICE")
 public interface ApiClient {
 	@GetMapping("/v1/department/getDepartmentByCode/{deptCode}")
 	DepartmentDto getDepartment(@PathVariable String deptCode);
